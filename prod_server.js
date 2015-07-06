c0c1fce6cb24987a652b0a564da5f6c7ab48c585
osaario <olli-matti.saario@aalto.fi>
2015-07-06 17:39:14 +0300

    heroku conf 3

--- prod_server.js
+++ prod_server.js
@@ -1,3 +1,3 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(443);
+connect().use(serveStatic(__dirname)).listen(process.ENV.port);
