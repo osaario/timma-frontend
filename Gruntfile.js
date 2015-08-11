396cbaa5431a2926fd3e5265f78caa47e0b846b1
osaario <olli-matti.saario@aalto.fi>
2015-08-11 18:10:21 +0300

    uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,15 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'js/app.js',
+        dest: 'js/app.min.js'
+      }
+    },
     browserify: {
      dist: {
        options: {
@@ -25,9 +34,11 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-shell');
 
-  grunt.registerTask('default', ['browserify']);
+  grunt.registerTask('default', ['jshint', 'browserify']);
+  grunt.registerTask('build-release', ['jshint', 'browserify', 'uglify']);
 
 };
